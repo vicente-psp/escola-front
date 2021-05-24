d35c5b4bd4484654e7c06781c3d8a204e8c9af66
Vicente Silva <vicente.pspenha@gmail.com>
2021-05-24 18:33:13 -0300

    Criado serviço para listar peso das avaliações.

---
+++ src/app/pages/avaliacao-peso/avaliacao-peso.service.ts
@@ -0,0 +1,29 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+import { Observable } from 'rxjs';
+
+import { environment } from './../../../environments/environment';
+
+
+export interface AvaliacaoPeso {
+  id: number;
+  descricao: string;
+  valor: number;
+}
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AvaliacaoPesoService {
+
+  private API_URL = `${environment.API_ENDPOINT}/avaliacoes-peso`;
+
+  constructor(private httpClient: HttpClient) { }
+
+  public listarFrequencia(): Observable<Array<AvaliacaoPeso>> {
+    return this.httpClient.get<Array<AvaliacaoPeso>>(this.API_URL);
+  }
+
+}
